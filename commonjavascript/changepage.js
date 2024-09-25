923799ec6bf25ce8503eb69fec47b623581bc1c6
absarker <absarker238@gmail.com>
2024-09-25 13:37:01 +0600

    added by blog and home button

--- commonjavascript/changepage.js
+++ commonjavascript/changepage.js
@@ -9,3 +9,19 @@ document.getElementById('btn-show-history').addEventListener('click',function(){
    
     
 })
+
+// home page section
+document.getElementById('blog-section').addEventListener('click',function(){
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('blog-section').classList.add('hidden');
+    document.getElementById('home-page-section').classList.remove('hidden');
+    document.getElementById('home-section').classList.remove('hidden');
+
+})
+document.getElementById('home-section').addEventListener('click',function(){
+    document.getElementById('donation-section').classList.remove('hidden');
+    document.getElementById('blog-section').classList.remove('hidden');
+    document.getElementById('home-page-section').classList.add('hidden');
+    document.getElementById('home-section').classList.add('hidden');
+
+})
---
+++ commonjavascript/setstyle.js
@@ -0,0 +1,22 @@
+const donationTab=document.getElementById('btn-show-donation');
+donationTab.addEventListener('click',function(){
+    donationTab.classList.add('bg-[#B4F461]', 'text-[#111111]');
+    document.getElementById('btn-show-history').classList.add( 'text-[#111111B3]')
+    donationTab.classList.remove('border-2')
+    document.getElementById('btn-show-history').classList.remove('bg-[#B4F461]', 'text-[#111111]')
+    document.getElementById('btn-show-history').classList.add('border-2')
+})
+
+
+
+//history section
+const historyTab = document.getElementById('btn-show-history');
+historyTab.addEventListener('click', function() {
+    historyTab.classList.add('bg-[#B4F461]', 'text-[#111111]');
+    document.getElementById('btn-show-donation').classList.add( 'text-[#111111B3]')
+    historyTab.classList.remove('border-2')
+    document.getElementById('btn-show-donation').classList.remove('bg-[#B4F461]', 'text-[#111111]')
+    document.getElementById('btn-show-donation').classList.add('border-2')
+ 
+
+});
